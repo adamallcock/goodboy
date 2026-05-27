eac9e2eea38dd8dc3d6fd828b13be8aa1e01a1af
Adam Allcock <adamallcock@gmail.com>
2026-05-27 00:34:50 -0400

    Add package exports and JSON helpers

---
+++ src/goodboy/__init__.py
@@ -0,0 +1,11 @@
+"""Goodboy pet production pipeline."""
+
+from .contracts import CELL_HEIGHT, CELL_WIDTH, ROW_FRAME_COUNTS, STATE_ORDER
+
+__all__ = [
+    "CELL_HEIGHT",
+    "CELL_WIDTH",
+    "ROW_FRAME_COUNTS",
+    "STATE_ORDER",
+]
+
